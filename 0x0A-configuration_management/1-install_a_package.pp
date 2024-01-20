ebd2af24a92f893a9cc8932af560cacbcf004e0c
Mohammed Al-Asfar <elasfarc@gmail.com>
2024-01-20 03:26:54 +0200

    [puppet] _ruby/puppet/util.rb:315: warning: deprecated Object#=~ is called on Puppet::Transaction::Report; it always returns nil

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# Using Puppet, install flask from pip3.
+# version 2.1.0
+
+
+package {'python3-pip':
+  ensure => 'installed'
+}
+
+package {'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
